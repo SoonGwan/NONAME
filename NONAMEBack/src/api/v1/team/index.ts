d43923988ed636cd5816175a7124d95f758df966
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-03 09:09:22 +0900

    [ADD] post make Team

--- NONAMEBack/src/api/v1/team/index.ts
+++ NONAMEBack/src/api/v1/team/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import getTeam from './getTeam';
+import makeTeam from './makeTeam';
 
 const router = Router();
 router.get('/getTeam', getTeam);
+router.post('/makeTeam', makeTeam);
 
 export default router;
