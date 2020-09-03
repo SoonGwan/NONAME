e29b1c1e00ddafe60ee621e189e9d0cc7b564c54
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-04 08:41:05 +0900

    [ADD] Multer uplaod

--- NONAMEBack/src/api/v1/index.ts
+++ NONAMEBack/src/api/v1/index.ts
@@ -3,11 +3,13 @@ import auth from './auth';
 import user from './user';
 import item from './item';
 import team from './team';
+import upload from './upload';
 const router = Router();
 
 router.use('/auth', auth);
 router.use('/user', user);
 router.use('/item', item);
 router.use('/team', team);
+router.use('/upload', upload);
 
 export default router;
