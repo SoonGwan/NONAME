fd279fa0fdec88dee4a73a499d95d028ec4fa007
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-03 13:04:21 +0900

    [ADD] Nav link

--- NONAMEFront/src/components/common/Template/Template.js
+++ NONAMEFront/src/components/common/Template/Template.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Nav from '../Nav/Nav';
 
 const Template = ({ children }) => {
   return (
     <>
       <Header />
+      <Nav />
       <div>{children}</div>
       <Footer />
     </>
