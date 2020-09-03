fbc270783d32de039d2a8efde698669e0c46c4fc
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-03 10:31:58 +0900

    [ADD] Header set

--- NONAMEFront/src/components/common/Header/Header.js
+++ NONAMEFront/src/components/common/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Header.scss';
-
+import {} from 'react-icons';
 const Header = () => {
   return (
     <>
-      <div></div>
+      <div className="Header">
+        <div className="Header-menu"></div>
+        <div className="Header-Title"></div>
+        <div className="Header-Search"></div>
+      </div>
     </>
   );
 };
