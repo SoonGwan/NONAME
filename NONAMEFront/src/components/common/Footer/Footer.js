1224d61ea8ec4ed2442fd2a954eb008c9cc7f8f9
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-03 18:16:54 +0900

    [ADD] Footer

--- NONAMEFront/src/components/common/Footer/Footer.js
+++ NONAMEFront/src/components/common/Footer/Footer.js
@@ -9,6 +9,7 @@ const Footer = () => {
           <div className="Footer-Area-Title">
             같이가 가치를 만들다 <br /> 서로의 특별한 팀을 .
           </div>
+          <div className="Footer-Area-Made">무제 Made in 순관</div>
           <div className="Footer-Area-Icons"></div>
         </div>
       </div>
