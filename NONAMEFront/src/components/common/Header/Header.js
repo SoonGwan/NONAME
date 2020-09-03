e5cadcbd7b7cfddfc8b86688489cf0aa76e809ba
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-03 20:24:08 +0900

    [FIX] header

--- NONAMEFront/src/components/common/Header/Header.js
+++ NONAMEFront/src/components/common/Header/Header.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './Header.scss';
-import { RiSearchEyeFill, RiMenu2Fill } from 'react-icons/ri';
+import { RiLoginBoxFill, RiMenu2Fill } from 'react-icons/ri';
+import { CgProfile } from 'react-icons/cg';
+
 const Header = () => {
   return (
     <>
       <div className="Header">
         <div className="Header-Menu">
-          <RiMenu2Fill />
+          <RiLoginBoxFill />
         </div>
         <div className="Header-Title">
           <span>무제</span> 프로젝트
         </div>
         <div className="Header-Search">
-          <RiSearchEyeFill />
+          <CgProfile />
         </div>
       </div>
     </>
