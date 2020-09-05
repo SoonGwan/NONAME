feb0ea3400464b79b4dd381680f4a3783be602ec
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-05 17:22:00 +0900

    [FIX]  map temp connect

--- NONAMEFront/src/stores/TeamList/TeamListStore.js
+++ NONAMEFront/src/stores/TeamList/TeamListStore.js
@@ -4,19 +4,12 @@ import TeamListRepository from './TeamListRepository';
 
 @autobind
 class TeamListStore {
-  @observable teamList = [
-    {
-      idx: 3,
-      teamName: '순관이',
-      explain: '설명',
-      mainImage: '1112261503-15772-415110-92615-4971196898151414.png',
-      whoMade: '순관',
-    },
-  ];
+  @observable teamList = [];
 
   @action
   async handleTeamList() {
     const response = await TeamListRepository.handleTeamList();
+    console.group(response);
     this.teamList = await response.data.team;
     console.log(this.teamList);
     return response;
