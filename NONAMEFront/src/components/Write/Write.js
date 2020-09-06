03b5245eb8cbed90e0625e7b6d5638dd3d89b34b
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-06 10:59:27 +0900

    [ADD]

--- NONAMEFront/src/components/Write/Write.js
+++ NONAMEFront/src/components/Write/Write.js
@@ -1,39 +1,52 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import MarkdownIt from 'markdown-it';
 import MdEditor from 'react-markdown-editor-lite';
+import ReactMarkdown from 'react-markdown';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/atom-one-light.css';
 
 // import style manually
 import 'react-markdown-editor-lite/lib/index.css';
 
 const Write = () => {
-  //   let mdEditor = null;
-  //   mdParser = null;
+  const mdEditor = useRef(null);
+  const [value, setValue] = useState();
+
   const handleEditorChange = ({ html, text }) => {
-    console.log('handleEditorChange', html, text);
+    const newValue = text.replace(/\d/g, '');
+    console.log(newValue);
+    setValue(newValue);
+    //
+    //
   };
-  const mdParser = new MarkdownIt({
-    html: true,
-    linkify: true,
-    typographer: true,
-    highlight: function (str, lang) {
-      if (lang && hljs.getLanguage(lang)) {
-        try {
-          return hljs.highlight(lang, str).value;
-        } catch (__) {}
-      }
-      return ''; // use external default escaping
-    },
-  });
   return (
     <>
       <div>글쓰기</div>
-      <MdEditor
+      {/* <MdEditor
         // ref={(node) => (mdEditor = node)}
         value=""
         style={{ height: '500px' }}
         renderHTML={(text) => mdParser.render(text)}
         // renderHTML={this.renderHTML}
         onChange={handleEditorChange}
+        config={{
+          view: {
+            menu: true,
+            md: true,
+            html: true,
+          },
+        }}
+      /> */}
+      <MdEditor
+        ref={mdEditor}
+        value={value}
+        style={{
+          width: '640px',
+          height: '500px',
+          margin: 'auto',
+        }}
+        onChange={handleEditorChange}
+        renderHTML={(text) => <ReactMarkdown source={text} />}
         config={{
           view: {
             menu: true,
