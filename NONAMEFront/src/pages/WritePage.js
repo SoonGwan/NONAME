5c6a066f6897ae4c26769906de3028c560d25ebe
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-06 16:16:39 +0900

    [ADD] write page

--- NONAMEFront/src/pages/WritePage.js
+++ NONAMEFront/src/pages/WritePage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Template from 'components/common/Template/Template';
 import Write from 'components/Write/Write';
+import TeamMakeContainer from 'container/TeamMakeContainer/TeamMakeContainer';
 
 const MainPage = () => {
   return (
     <Template>
-      <Write />
+      <TeamMakeContainer />
     </Template>
   );
 };
--- NONAMEFront/src/stores/index.js
+++ NONAMEFront/src/stores/index.js
@@ -1,9 +1,13 @@
 import AuthStore from './Auth';
 import TeamListStore from './TeamList/TeamListStore';
+import TeamMakeStore from './TeamMake/TeamMakeStore';
+import uploadStore from './upload/uploadStore';
 
 const stores = {
   AuthStore: new AuthStore(),
   TeamListStore: new TeamListStore(),
+  TeamMakeStore: new TeamMakeStore(),
+  uploadStore: new uploadStore(),
 };
 
 export default stores;
