7d4dcc36bbf6b1a58d4423c1a00a0904a88ccf02
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-06 12:14:33 +0900

    [ADD] img Upload

---
+++ NONAMEFront/src/stores/upload/uploadRepository.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { SERVER } from 'config/config.json';
+
+class uploadRepository {
+  handleUpLoad = async (request) => {
+    const formData = new FormData();
+    formData.append('files', request);
+
+    const { data } = await axios.post(`${SERVER}/upload`, formData);
+
+    return data;
+  };
+}
+
+export default new uploadRepository();
---
+++ NONAMEFront/src/stores/upload/uploadStore.js
@@ -0,0 +1,17 @@
+import { action, observable } from 'mobx';
+import { autobind } from 'core-decorators';
+import uploadRepository from './uploadRepository';
+
+@autobind
+class uploadStore {
+  @observable imgURL = '';
+
+  @action
+  async handleUpLoad(request) {
+    const response = await uploadRepository.handleUpLoad(request);
+    this.imgURL = await response.data.files[0];
+    return response;
+  }
+}
+
+export default uploadStore;
