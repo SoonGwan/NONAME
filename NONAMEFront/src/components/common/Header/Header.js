38bd7646d07c16e28f85b21d1777675cf9a4adbe
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-07 11:18:54 +0900

    [ADD] header

--- NONAMEFront/src/components/common/Header/Header.js
+++ NONAMEFront/src/components/common/Header/Header.js
@@ -3,14 +3,19 @@ import './Header.scss';
 import { RiLoginBoxFill, RiMenu2Fill } from 'react-icons/ri';
 import { CgProfile } from 'react-icons/cg';
 import { useHistory } from 'react-router-dom';
+import AuthContainer from 'container/AuthContainer/AuthContainer';
+import Login from 'components/Auth/Auth.Login';
+import { observer } from 'mobx-react';
+import useStore from 'lib/useStores';
 
-const Header = () => {
-  let history = useHistory();
+const Header = observer(() => {
+  const { store } = useStore();
+  const { isLoginModal, handleLogin, handleLoginModal } = store.AuthStore;
   return (
     <>
       <div className="Header">
         <div className="Header-Menu">
-          <RiLoginBoxFill onClick={() => history.push('/login')} />
+          <RiLoginBoxFill onClick={() => handleLoginModal()} />
         </div>
         <div className="Header-Title">
           <span>무제</span> 프로젝트
@@ -19,8 +24,9 @@ const Header = () => {
           <CgProfile />
         </div>
       </div>
+      <AuthContainer />
     </>
   );
-};
+});
 
 export default Header;
