b4ff986e25878ea503622e5ea17a618b66565600
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-07 15:46:33 +0900

    [ADD] modal cancel button

--- NONAMEFront/src/components/common/Modal/Modal.js
+++ NONAMEFront/src/components/common/Modal/Modal.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Modal.scss';
+import { AiFillCloseSquare } from 'react-icons/ai';
 const Modal = ({ handleClose, width, height, children }) => {
   return (
     <>
       <div className="Modal">
         <div className="Modal-PostBackGround" onClick={handleClose}></div>
         <div className="Modal-Wrap" style={{ width: width, height: height }}>
+          <div className="Modal-Cancel">
+            <AiFillCloseSquare
+              onClick={() => handleClose()}
+              style={{ cursor: 'pointer', width: '20px', height: '20px' }}
+            />
+            <hr />
+          </div>
+          <div className="Modal-Item">여기 아이템</div>
           {children}
         </div>
       </div>
