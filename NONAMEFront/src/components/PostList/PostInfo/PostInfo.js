9248f81371f11b8963280aee7e81c1c71f2cd5d9
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-07 11:44:38 +0900

    [ADD] PostInfo

---
+++ NONAMEFront/src/components/PostList/PostInfo/PostInfo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PostInfo.scss';
+
+const PostInfo = () => {
+  return (
+    <>
+      <div className="PostInfo">
+        <div className="PostInfo-Wrap">인포 랩</div>
+      </div>
+    </>
+  );
+};
+
+export default PostInfo;
