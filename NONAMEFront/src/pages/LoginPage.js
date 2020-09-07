202f207026d30288ae07d042a91360600e50ccad
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-07 10:14:33 +0900

    [ADD] login connect

---
+++ NONAMEFront/src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Template from 'components/common/Template/Template';
+import Login from 'components/Auth/Auth.Login/Login';
+const LoginPage = () => {
+  return (
+    <>
+      <Template>
+        <Login />
+      </Template>
+    </>
+  );
+};
+
+export default LoginPage;
