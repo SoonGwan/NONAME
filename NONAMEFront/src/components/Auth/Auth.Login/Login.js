361682f1dab964ced822bf79ffd891d298290eb5
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-07 19:38:49 +0900

    [ADD] Login Regitser

--- NONAMEFront/src/components/Auth/Auth.Login/Login.js
+++ NONAMEFront/src/components/Auth/Auth.Login/Login.js
@@ -37,13 +37,40 @@ const Login = ({ handleLoginModal }) => {
         ) : (
           <>
             <div className="Register">
-              <div className="Regitser-WrapID">
-                <p>너의 아이디를 입력해줘</p>
-                <input
-                  className="Regitser-WrapID-ID"
-                  type="text"
-                  placeholder="여기야 여기"
-                />
+              <div className="Register-Title">진짜로 가입 할거죠?</div>
+              <div
+                style={{
+                  display: 'flex',
+                  marginTop: '15px',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <div className="Register-WrapID">
+                  <p>너의 이름을 입력해줘</p>
+                  <input
+                    className="Register-WrapID-ID"
+                    type="text"
+                    placeholder="내 이름은 순관이야"
+                  />
+                </div>
+                <div className="Register-WrapID">
+                  <p>너의 아이디를 입력해줘</p>
+                  <input
+                    className="Register-WrapID-ID"
+                    type="text"
+                    placeholder="여기야 여기"
+                  />
+                </div>
+              </div>
+              <div>
+                <div className="Register-WrapEmail">
+                  <p>너의 이메일을 입력해줘</p>
+                  <input
+                    className="Register-WrapEmail-Email"
+                    type="email"
+                    placeholder="나는 구글을 좋아해"
+                  />
+                </div>
               </div>
             </div>
           </>
