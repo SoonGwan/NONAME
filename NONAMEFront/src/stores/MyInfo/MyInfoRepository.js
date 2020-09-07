da38fb314e6a9c82ae9d9265a08f22953fdd47ed
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-07 09:27:39 +0900

    [ADD] myinfo

---
+++ NONAMEFront/src/stores/MyInfo/MyInfoRepository.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { SERVER } from 'config/config.json';
+
+class MyInfoRepository {
+  myInfo = async () => {
+    const { data } = await axios.get(`${SERVER}/user/myInfo`);
+
+    return data;
+  };
+}
+
+export default new MyInfoRepository();
