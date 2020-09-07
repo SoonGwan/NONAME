ab520fc2ce95092588f6eb5d5f056255e501895a
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-07 12:10:16 +0900

    [ADD] Modal

---
+++ NONAMEFront/src/components/common/Modal/Modal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Modal.scss';
+const Modal = ({ handleClose, width, height, children }) => {
+  return (
+    <>
+      <div className="Modal">
+        <div className="Modal-PostBackGround" onClick={handleClose}></div>
+        <div className="PostInfo-Wrap" style={{ width: width, height: height }}>
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
