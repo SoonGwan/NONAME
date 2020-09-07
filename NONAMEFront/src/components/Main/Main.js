9bb248b106212ccf1f31129dd1bf5783ee978ffd
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-07 12:10:41 +0900

    [ADD] TeamInfo Container Modal

--- NONAMEFront/src/components/Main/Main.js
+++ NONAMEFront/src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Main.scss';
 import PostList from 'components/PostList/PostList';
+import TeamInfoContainer from 'container/TeamListContainer/TeamInfoContainer';
 
 const Main = ({ ListMap }) => {
   return (
     <>
       <div className="Main">{ListMap}</div>
+      <TeamInfoContainer />
     </>
   );
 };
