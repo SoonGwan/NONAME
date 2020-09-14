7e8da9ade508db054b4c30b24e50cee260112d9b
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-14 13:19:14 +0900

    [CHANGE] styled-components

---
+++ NONAMEFront/src/components/common/Footer/Fotter.style.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const FooterStyles = styled.div`
+  margin: auto;
+  width: 640px;
+  height: 148px;
+  background-color: #f2f2f2;
+`;
+
+export const FooterArea = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 15px 20px 0px 20px;
+  flex-direction: column;
+`;
+
+export const FooterTitle = styled.div`
+  color: #969696;
+  font-weight: 600;
+  font-size: 13.8px;
+`;
+
+export const FooterMade = styled.div`
+  color: #b3aeae;
+  font-weight: 600;
+  font-size: 13.8px;
+  position: relative;
+  margin-top: 12px;
+`;
