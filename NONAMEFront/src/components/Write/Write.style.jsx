d03a8296b4dec52d530c87693f10a044e275cbc8
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-15 13:19:35 +0900

    [ADD] write

---
+++ NONAMEFront/src/components/Write/Write.style.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const WriteStyles = styled.div`
+  width: 640px;
+  height: 100%;
+  margin: auto;
+`;
+
+export const TeamNameTitle = styled.div``;
+
+export const TeamName = styled.input`
+  border: none;
+  width: 240px;
+  height: 50px;
+`;
+
+export const FileInput = styled.input`
+  border: none;
+  width: 240px;
+  height: 50px;
+`;
+
+export const FileLabel = styled.label``;
