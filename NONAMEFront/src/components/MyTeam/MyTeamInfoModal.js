d10809239730ff0793d756f81e16be06622ebed2
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-26 00:36:04 +0900

    [ADD] TeamApplyUserList View

--- NONAMEFront/src/components/MyTeam/MyTeamInfoModal.js
+++ NONAMEFront/src/components/MyTeam/MyTeamInfoModal.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Modal from 'components/common/Modal/Modal';
 
-const MyTeamInfoModal = ({ handleMyTeamInfoModal }) => {
+const MyTeamInfoModal = ({ handleMyTeamInfoModal, userList }) => {
   return (
     <>
       <Modal handleClose={handleMyTeamInfoModal}>
-        <div>asdasdasd</div>
+        <div>{userList}</div>
       </Modal>
     </>
   );
