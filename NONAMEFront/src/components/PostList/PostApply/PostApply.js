fbee70c99c6cb09581d2de0fd82a71c515f22824
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-28 10:12:44 +0900

    [ADD] Applymodal

--- NONAMEFront/src/components/PostList/PostApply/PostApply.js
+++ NONAMEFront/src/components/PostList/PostApply/PostApply.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Modal from 'components/common/Modal/Modal';
 
-const PostApply = () => {
+const PostApply = ({
+  handleApplyModal,
+  isPress,
+  team_apply_reason,
+  setTeam_apply_reason,
+  team_connect,
+  setTeam_connect,
+  requestHandleApplyTeam,
+}) => {
   return (
     <>
-      <Modal width={'480px'} height={'480px'}>
-        sdfs
+      <Modal
+        width={'480px'}
+        height={'480px'}
+        handleClose={handleApplyModal}
+        isPress={isPress}
+      >
+        <input
+          type="text"
+          value={team_apply_reason}
+          onChange={(e) => setTeam_apply_reason(e.target.value)}
+        />
+        <input
+          type="text"
+          value={team_connect}
+          onChange={(e) => setTeam_connect(e.target.value)}
+        />
+        <button onClick={() => requestHandleApplyTeam()}>button</button>
       </Modal>
     </>
   );
--- NONAMEFront/src/container/TeamListContainer/TeamApplyContainer.js
+++ NONAMEFront/src/container/TeamListContainer/TeamApplyContainer.js
@@ -2,28 +2,60 @@ import React, { useEffect, useCallback, useState } from 'react';
 import { observer } from 'mobx-react';
 import useStore from 'lib/useStores';
 import PostModal from 'components/PostList/PostApply/PostApply';
+import { ToastContainer, toast } from 'react-toastify';
+
 const TeamApplyContainer = observer(() => {
+  toast.configure();
+  const [team_apply_reason, setTeam_apply_reason] = useState('');
+  const [team_connect, setTeam_connect] = useState('');
   const { store } = useStore();
-  const { handleApplyTeam, applyModal } = store.TeamApply;
+  const { handleApplyTeam, applyModal, handleApplyModal } = store.TeamApply;
   const { idx } = store.TeamListStore;
+  console.log(idx);
   const requestHandleApplyTeam = useCallback(async () => {
+    if (!localStorage.getItem('name')) {
+      toast.error('로그인 후 사용해주세요.', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+
+      return;
+    }
+
     const data = {
       user_id: localStorage.getItem('name'),
       apply_team_idx: idx,
       team_apply_reason,
+      team_connect,
     };
+
     try {
       const response = await handleApplyTeam(data);
       console.log(response);
+      handleApplyModal();
     } catch (error) {
       console.log(error);
+
       return error;
     }
   });
   return (
     <>
       {applyModal && (
-        <PostModal requestHandleApplyTeam={requestHandleApplyTeam} />
+        <PostModal
+          handleApplyModal={handleApplyModal}
+          isPress={applyModal}
+          requestHandleApplyTeam={requestHandleApplyTeam}
+          team_apply_reason={team_apply_reason}
+          setTeam_apply_reason={setTeam_apply_reason}
+          team_connect={team_connect}
+          setTeam_connect={setTeam_connect}
+        />
       )}
     </>
   );
