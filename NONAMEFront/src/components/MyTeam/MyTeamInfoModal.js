e58389d59b7dcfa9846e44e0eeec2840825e553d
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-10-06 08:28:34 +0900

    [ADD] TeamInfoModal

--- NONAMEFront/src/components/MyTeam/MyTeamInfoModal.js
+++ NONAMEFront/src/components/MyTeam/MyTeamInfoModal.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import Modal from 'components/common/Modal/Modal';
+import page404 from 'components/assets/img/page404.png';
 
-const MyTeamInfoModal = ({ handleMyTeamInfoModal, userList }) => {
+import {
+  PostInfoStlyes,
+  Thumbnail,
+  TeamName,
+  WhoMade,
+  ContentWrap,
+  ButtonWrap,
+  DefaultButton,
+  ApplyModalButton,
+} from './MyTeamInfoModal.style';
+import { IMG } from 'config/config.json';
+
+const MyTeamInfoModal = ({
+  handleMyTeamInfoModal,
+  userList,
+  teamName,
+  whoMade,
+  mainImage,
+  explain,
+  idx,
+}) => {
   return (
     <>
       <Modal handleClose={handleMyTeamInfoModal}>
+        <PostInfoStlyes>
+          <Thumbnail
+            src={
+              mainImage.includes('.png', '.jpeg', '.jpg', '.svg')
+                ? IMG + mainImage
+                : page404
+            }
+            alt=""
+          />
+          <TeamName>프로젝트 이름: {teamName}</TeamName>
+          <WhoMade>만든 사람 : {whoMade}</WhoMade>
+        </PostInfoStlyes>
         <div>{userList}</div>
       </Modal>
     </>
