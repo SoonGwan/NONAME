f0f5f82ef208b9b4e420055470df09af60a15228
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-10-22 11:46:53 +0900

    [ADD] MyTeamDelete Api

--- NONAMEBack/src/api/v1/team/index.ts
+++ NONAMEBack/src/api/v1/team/index.ts
@@ -4,12 +4,15 @@ import makeTeam from './makeTeam';
 import getMyTeam from './getMyTeamList';
 import infoTeam from './infoTeam';
 import modifyTeam from './ModifyTeam';
+import deleteMyTeam from './deleteMyTeam';
 
 const router = Router();
+
 router.get('/getTeam', getTeam);
 router.post('/makeTeam', makeTeam);
 router.get('/getMyTeam', getMyTeam);
 router.get('/infoTeam', infoTeam);
 router.put('/modifyTeam', modifyTeam);
+router.delete('/deleteMyTeam', deleteMyTeam);
 
 export default router;
