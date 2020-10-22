1f651dc6545608055c46966befebc657a860bc44
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-10-22 11:52:19 +0900

    [ADD] ModifyContainer logic

--- NONAMEFront/src/container/MyTeamContainer/MyTeamModifyContainer.js
+++ NONAMEFront/src/container/MyTeamContainer/MyTeamModifyContainer.js
@@ -4,7 +4,7 @@ import { observer } from 'mobx-react';
 import Write from 'components/Write/Write';
 import Modal from 'components/common/Modal/Modal';
 import ModiftyWrite from 'components/Write/ModifyWrite';
-
+import { ToastContainer, toast } from 'react-toastify';
 const MyTeamModifyContainer = observer(() => {
   const { store } = useStore();
   const {
@@ -42,8 +42,58 @@ const MyTeamModifyContainer = observer(() => {
 
       try {
         const response = await handleModifyMyTeam(request, idx);
-        console.log(response);
+        const { status } = response;
+        if (status === 200) {
+          toast.success('성공적으로 수정을 하였습니다.', {
+            position: 'top-right',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+          handleMyTeamInfoModal();
+          handleModifyTeamModal();
+          handleMyTeamList();
+          return;
+        }
       } catch (error) {
+        const { status } = error;
+        if (status === 400) {
+          toast.error('글이 터엉 비어있어요!', {
+            position: 'top-right',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+          return;
+        } else if (status === 404) {
+          toast.error('존재하지 않는 팀입니다!', {
+            position: 'top-right',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+          return;
+        } else if (status === 500) {
+          toast.error('서버 오류!', {
+            position: 'top-right',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+          return;
+        }
         return error;
       }
     },
